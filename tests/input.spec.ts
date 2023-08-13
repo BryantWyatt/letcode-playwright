144e4d5bcefe6f0c9620a392f5570d7dbf1d7b47
Bryant Wyatt <bryant.wyatt@gmail.com>
2023-08-13 16:56:45 -0400

    Add prettier

--- tests/input.spec.ts
+++ tests/input.spec.ts
@@ -1,79 +1,81 @@
-import { test, expect, type Page } from '@playwright/test';
+import { test, expect, type Page } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('https://letcode.in/edit');
+  await page.goto("https://letcode.in/edit");
 });
 
-test.describe('Input', () => {
-    test('should accept full name', async ({ page }) => {
-        // Arrange
-        const name = "John Example Smith";
-        const fullNameInput = await page.getByPlaceholder("Enter first & last name");
+test.describe("Input", () => {
+  test("should accept full name", async ({ page }) => {
+    // Arrange
+    const name = "John Example Smith";
+    const fullNameInput = await page.getByPlaceholder(
+      "Enter first & last name",
+    );
 
-        // Act
-        await fullNameInput.fill(name);
+    // Act
+    await fullNameInput.fill(name);
 
-        // Assert
-        await expect(fullNameInput).toHaveValue(name);
-    });
+    // Assert
+    await expect(fullNameInput).toHaveValue(name);
+  });
 
-    test("should append text", async ({page}) => {
-        // Arrange
-        const input = await page.locator('#join');
+  test("should append text", async ({ page }) => {
+    // Arrange
+    const input = await page.locator("#join");
 
-        // Act
-        await input.focus();
-        await page.keyboard.press('End');
-        await input.type(". How are you?");
+    // Act
+    await input.focus();
+    await page.keyboard.press("End");
+    await input.type(". How are you?");
 
-        // Assert
-        expect(input).toHaveValue("I am good. How are you?")
-    });
+    // Assert
+    expect(input).toHaveValue("I am good. How are you?");
+  });
 
-    test("should accept tab key", async ({page}) => {
-        // Arrange
-        const input = await page.locator('#join');
+  test("should accept tab key", async ({ page }) => {
+    // Arrange
+    const input = await page.locator("#join");
 
-        // Act
-        await input.focus();
-        await page.keyboard.press('Tab');
+    // Act
+    await input.focus();
+    await page.keyboard.press("Tab");
 
-        // Assert
-        expect(page.locator('#getMe')).toBeFocused();
-    });
+    // Assert
+    expect(page.locator("#getMe")).toBeFocused();
+  });
 
-    test("should have correct content", async ({page}) => {
-        // Arrange
-        const input = await page.locator('#getMe');
+  test("should have correct content", async ({ page }) => {
+    // Arrange
+    const input = await page.locator("#getMe");
 
-        // Assert
-        expect(input).toHaveValue('ortonikc');
-    });
+    // Assert
+    expect(input).toHaveValue("ortonikc");
+  });
 
-    test("should be clearable", async ({page}) => {
-        // Arrange
-        const input = await page.locator('#clearMe');
+  test("should be clearable", async ({ page }) => {
+    // Arrange
+    const input = await page.locator("#clearMe");
 
-        // Act
-        await input.clear();
+    // Act
+    await input.clear();
 
-        // Assert
-        expect(input).toHaveValue("");
-    });
+    // Assert
+    expect(input).toHaveValue("");
+  });
 
-    test("should be disabled", async ({page}) => {
-        // Arrange
-        const input = await page.locator('#noEdit');
+  test("should be disabled", async ({ page }) => {
+    // Arrange
+    const input = await page.locator("#noEdit");
 
-        // Assert
-        await expect(input).toBeDisabled();
-    })
+    // Assert
+    await expect(input).toBeDisabled();
+  });
 
-    test("should be read-only", async({page}) => {
-        // Arrange
-        const input = await page.locator('#dontwrite');
+  test("should be read-only", async ({ page }) => {
+    // Arrange
+    const input = await page.locator("#dontwrite");
 
-        // Assert
-        await expect(input).toHaveAttribute("readonly", "");
-    })
+    // Assert
+    await expect(input).toHaveAttribute("readonly", "");
+  });
 });
